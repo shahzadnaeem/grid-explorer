d6cda208f07c1a3bf290e8f667eb09f6a24eba7e
Shahzad Naeem <shahzad65naeem@gmail.com>
2023-03-19 13:19:49 +0000

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./css/reset.css";
+import "./css/utils.css";
+import "./css/debug.css";
+
+import "./App.css";
+
+function WrapperControls(props) {
+  return (
+    <div className="wrapper-controls grid-cols just-middle">
+      <button className="wrapper-button" onClick={props.toggleDebug}>
+        {"CSS Debug " + (props.debug ? "🟢" : "🔴")}
+      </button>
+    </div>
+  );
+}
+
+function Wrapper(props) {
+  const [debug, setDebug] = useState(false);
+
+  const toggleDebug = () => {
+    setDebug((d) => {
+      console.log(`setDebug: ${d} => ${!d}`);
+      return !d;
+    });
+  };
+
+  return (
+    <div className={"wrapper grid-1col" + (debug ? " __CSSDEBUG__" : "")}>
+      <WrapperControls debug={debug} toggleDebug={toggleDebug} />
+      {props.children}
+    </div>
+  );
+}
+
+function Box(props) {
+  return <div className="box pad1">{props.children}</div>;
+}
+
+function Grid(props) {
+  const boxes = new Array(props.numBoxes).fill(0);
+
+  return (
+    <div className="grid grid-1col pad4">
+      <div className={props.shape}>
+        {boxes.map((b, i) => (
+          <Box key={i}>{`Box ${i}`}</Box>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <main className="app-base">
+        <Grid shape="grid-8cols" numBoxes={10} />
+        <Grid shape="grid-8rows" numBoxes={4} />
+      </main>
+    </Wrapper>
   );
 }
 
