f97032e8050151abc19382e3f753caf1d51f4959
Shahzad Naeem <shahzad65naeem@gmail.com>
2023-03-25 14:58:36 +0000

    Better Help

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -4,26 +4,29 @@ export default function Help() {
   return (
     <div className="Help">
       <h1>Help</h1>
-      <p>Select a Layout from the dropdown list</p>
-      <p>Adjust the options to see what effect they have</p>
-      <p>
-        You can adjust the <i>Height</i> to allow you to better see the impact
-        of the <i>spc-</i> options of <i>Align Content</i>
-      </p>
-      <p>
-        If you choose a <i>flexbox</i> layout you can also try the <i>wrap</i>{" "}
-        option - you will likely need to adjust the <i>Height</i> so that
-        wrapping is required
-      </p>
-      <hr />
-      <p>You can also add your own Layouts and Options</p>
-      <p>
-        <span>src/App.js</span> is where the Layouts are defined
-      </p>
-      <p>
-        <span>src/components/Grid/Grid.css</span> is where the CSS for Options
-        are defined - the <i>Selected options: </i> list
-      </p>
+      <ul>
+        <li>Select a Layout from the dropdown list above</li>
+        <li>Adjust the options to see what effect they have</li>
+        <li>
+          You can adjust the <i>Height</i> to allow you to better see the impact
+          of the <i>Align Content</i> <i>spc-</i> options
+        </li>
+        <li>Similarly, you can add and remove Boxes and/or Xtra Boxes</li>
+        <li>
+          If you choose a <i>flexbox</i> layout you can also try the <i>wrap</i>{" "}
+          option - you will likely need to adjust the <i>Height</i> so that
+          wrapping is required
+        </li>
+        <hr />
+        <li>You can also add your own Layouts and Options</li>
+        <p>
+          <span>src/App.js</span> is where the Layouts are defined
+        </p>
+        <p>
+          <span>src/components/Grid/Grid.css</span> is where the CSS for Options
+          are defined - the <i>Selected options: </i> list
+        </p>
+      </ul>
     </div>
   );
 }
